aa430149a79c38ed6ddf8bf2bf48ab40f8a1b021
EuraKo <sow5252@gmail.com>
2022-04-26 11:49:42 +0900

    gallery ing

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -6,16 +6,29 @@ const items = list.querySelectorAll('li');
 items.forEach((el, idx) => {
   el.addEventListener('click', e => {
     e.preventDefault();
-    showPop("src/img/HC-About-2.jpg", 'title', 'desc', "https://www.flickr.com/images/buddyicon.gif", 'owner_name');
+    showPop("src/img/HC-About-2.jpg", 'title', 'fsdjksjdfdfkjldskljdsfkljsfkljsdfklj', "https://www.flickr.com/images/buddyicon.gif", 'owner_name');
   })
 });
+body.addEventListener('click', e => {
+  let pop = body.querySelector('.pop');
+  if (pop !== null) {
+    let close = pop.querySelector('.pop_cancle');
+    let closeSpan = close.querySelector('span');
+    // 좀 더 간결한 코드?
+    if (e.target === close || e.target === closeSpan) {
+      pop.remove();
+      body.classList.remove('scroll_hidden');
+    }
+  }
+})
 
 function showPop(img, title, desc, owner_img, owner_name) {
+  body.classList.add('scroll_hidden');
   const pop = document.createElement('aside');
   pop.classList.add('pop');
   let htmls = `
     <div class="inner">
-      <button type="button" class="pop_cancle">x</button>
+      <button type="button" class="pop_cancle"><span>close</span></button>
       <div class="pop_cont">
         <div class="pic">
           <img src="${img}">
@@ -32,6 +45,10 @@ function showPop(img, title, desc, owner_img, owner_name) {
     </div>
   `;
   pop.innerHTML = htmls;
-  // body.append(pop);
+  body.append(pop);
+
+}
+
+function hidePop() {
 
 }
